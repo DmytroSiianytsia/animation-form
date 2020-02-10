15aff8a5806a448aaa29395d5cbde43ada9a3eec
Dima <ds081181@gmail.com>
2020-02-10 23:34:43 +0200

    TODO

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const page = document.getElementById('container');
 
 window.addEventListener('DOMContentLoaded', () => {
    Promise.all([
-       page.style.backgroundImage = './img/1.jpg',
-       page.style.backgroundImage = './img/2.jpg',
-       page.style.backgroundImage = './img/3.jpg',
-       page.style.backgroundImage = './img/4.jpg',
-       page.style.backgroundImage = './img/5.jpg'
+       fetch('./img/1.jpg'),
+       fetch('./img/2.jpg'),
+       fetch('./img/3.jpg'),
+       fetch('./img/4.jpg'),
+       fetch('./img/5.jpg')
    ])
-
-       .then(result => console.log(result))
+       .then(result => result.map(item => {
+           console.log(item.url);
+           page.style.backgroundImage = `url(${item.url})`
+       }))
 });
-
-
