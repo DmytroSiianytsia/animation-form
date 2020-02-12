7919a051fe1b9fc7b50a531e22ed78fcc1beac57
Dima <ds081181@gmail.com>
2020-02-12 14:31:03 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+if (document.images) {
+    img1 = new Image();
+    img2 = new Image();
+    img3 = new Image();
+    img4 = new Image();
+    img5 = new Image();
+
+    img1.src = "./img/1.jpg";
+    img2.src = "./img/2.jpg";
+    img3.src = "./img/3.jpg";
+    img4.src = "./img/4.jpg";
+    img5.src = "./img/5.jpg";    
+}
